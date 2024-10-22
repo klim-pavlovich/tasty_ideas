d2a3cb5fa795d2bf6fe1d95d5d0093583ca28a36
Klim < ->
2024-10-22 19:00:53 +0300

    added models: reciepe, category, recipecategory to models.py and added task.txt to main folder

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return f'Название: {self.name}'
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=5000)
+    coocking_steps = models.TextField(max_length=5000)
+    coocking_time = models.IntegerField() # в минутах
+    image_of_food = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    ingridients = models.TextField(max_length=2500, blank=True)
+    date_of_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Название: {self.title}, автор: {self.author}, дата создания: {self.date_of_create}"
+
+
+class RecipeCategory(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Рецепт: {self.recipe.title}, категория: {self.category.name}'
